3fd18985c99f9e4f4630e28215b0d359e564827f
sreenivas <vasujags@gmail.com>
2018-06-22 20:21:36 -0500

    "Added version number"

--- src/main/java/com/advertiser/application/AdvertiserApplication.java
+++ src/main/java/com/advertiser/application/AdvertiserApplication.java
@@ -8,7 +8,7 @@ import javax.ws.rs.core.Application;
 
 import com.advertiser.resource.AdvertiserRS;
 
-@ApplicationPath("/")
+@ApplicationPath("/v1")
 public class AdvertiserApplication extends Application
 {		
 	 @Override
